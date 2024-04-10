fee5eb51ccc6dba3db675367becf811c73a44b90
Teemu-Kostamo <kostis12@hotmail.com>
2024-04-11 00:05:59 +0300

    login_required lisätty main ja detail views:hin

--- lainatehtailijat/lainatehdas/views.py
+++ lainatehtailijat/lainatehdas/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserRegisterForm
 from .models import Item
 from django.http import Http404, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request): #Vaihda tämä pääsivuksi
-    return redirect('/main')
+def index(request):
+        return redirect('/main')
 
 def register(request):
     if request.method == 'POST':
@@ -17,11 +19,13 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'lainatehdas/register.html', {'form': form})
 
+@login_required(login_url="/login")
 def main(request):
     items_list = Item.objects.order_by("item_name")
     context = {'items_list' : items_list}
     return render(request, 'lainatehdas/main.html', context)
 
+@login_required(login_url="/login")
 def detail(request, item_id):
     item = get_object_or_404(Item, pk = item_id)
     return render(request, 'lainatehdas/detail.html', {'item' : item})
