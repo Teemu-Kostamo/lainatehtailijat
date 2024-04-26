df39e143c80ece1b458fda4859b3ef16ce51e6fb
Teemu-Kostamo <kostis12@hotmail.com>
2024-04-26 17:33:55 +0300

    lisätty 404 sivu

--- lainatehtailijat/lainatehtailijat/settings.py
+++ lainatehtailijat/lainatehtailijat/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
